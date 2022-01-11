85decd935de640ea06b276c1794b5b6fd7543f49
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-01-11 12:30:30 +0530

    Add files via upload
    adding files for generating manual

---
+++ ColourCoding_GenerateManual.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "Core_TelecomCableColourCoding.h"
+#include "Test_TelecomCableColourCoding.h"
+
+void ColourCoding_GenerateRefernceManual(void)
+{
+    int MajorColor = 0;
+    int MinorColor = 0;
+    int ColourCodingNumber = 1;
+
+    printf("\n\nPlease find the reference manual for the colour coding below \n\n");
+    printf("Major colour\t\tMinor colour\t\tColourCodingNumber \n");
+    for(MajorColor = 0; MajorColor < numberOfMajorColors; MajorColor++)
+    {
+        for(MinorColor = 0; MinorColor < numberOfMinorColors; MinorColor++)
+        {
+            printf("%s     \t\t%s       \t\t        %d      \n",MajorColorNames[MajorColor], MinorColorNames[MinorColor], ColourCodingNumber);
+            ColourCodingNumber++;
+        }
+    }
+}
