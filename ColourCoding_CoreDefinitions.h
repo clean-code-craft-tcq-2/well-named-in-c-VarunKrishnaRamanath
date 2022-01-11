305c46e3737e65460f1eaf092ec066005f8c4e0c
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-01-11 12:32:34 +0530

    Update ColourCoding_CoreDefinitions.h
    exporting the number of major and minor colours for manual generation

--- ColourCoding_CoreDefinitions.h
+++ ColourCoding_CoreDefinitions.h
@@ -24,6 +24,11 @@ typedef struct {
     MinorColor minorColor;
 } ColorPair;
 
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+extern const int numberOfMajorColors;
+extern const int numberOfMinorColors;
+
 extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
 extern ColorPair GetColorFromPairNumber(int pairNumber);
 extern int GetPairNumberFromColor(const ColorPair* colorPair);
